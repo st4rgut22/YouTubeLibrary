3c2f2abc81f6990782ea66732dea2c8188b52a90
edward cox <edwardcox@new-host-3.home>
2018-11-06 09:01:39 -0800

    now it can add playlists

--- Related_Videos.py
+++ Related_Videos.py
@@ -8,48 +8,71 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+options = Options()
+options.add_extension('/Users/edwardcox/chrome_extension/extension_1_5_21_0.crx')
 
+#chrome_options.add_extension('/Users/edwardcox/Desktop/extension_1_5_21_0.crx')
 
 class YoutubeSubtitlesScraper:
     def __init__(self,url):
+        self.subtitles_file = open("subtitleUrl.txt",'w')
         self.start_url = url
         
     def __enter__(self):
-        self.driver = webdriver.Chrome(executable_path='/Users/edwardcox/chromedriver/chromedriver')
-        
+        self.driver = webdriver.Chrome(executable_path='/Users/edwardcox/chromedriver/chromedriver',chrome_options=options)
         self.wait = WebDriverWait(self.driver, 10)
-
         return self
         
     # enter & exit similar to try, finally
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.driver.close()
 
-    def subtitles(self):
+    def playlist_all(self):
+        for i in range(1):
+            self.driver.get(self.start_url)
+            playlist_title_btns = self.wait.until(EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT,'VIEW FULL PLAYLIST')))
+            playlist_title_btn = playlist_title_btns[i]
+            single_playlist_url = playlist_title_btn.get_attribute('href')
+            playlist_title_btn.click()
+            self.playlist_single(single_playlist_url)
+        
+    def playlist_single(self,playlist_url):
+        playlist_single_btns = self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer')))
+        len_btns = len(playlist_single_btns)
+        for i in range(len_btns): #stale element exception, must reassign reference every time
+            playlist_single_btns = self.wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR,'.yt-simple-endpoint.style-scope.ytd-playlist-video-renderer')))
+            url = playlist_single_btns[i].get_attribute('href')
+            for title, link, channel in self.subtitles(url):
+                try: 
+                    self.subtitles_file.write(title + '\t' + link + '\t' + channel + '\n')
+                except Exception as e:
+                    print(e)
+                self.driver.get(playlist_url) #return to the previous page
+            
+    def subtitles(self,url):
         """Visits video's page, enables 'CC' to scrape the subtitles and generates filename, link and the subtitles content."""
-        for x in range(100):
-            try:                
-                self.driver.get(self.start_url)
-                WebDriverWait(self.driver,3).until(EC.presence_of_element_located((By.XPATH,"/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[2]/div/div[2]/ytd-watch-next-secondary-results-renderer/div[2]/ytd-compact-autoplay-renderer/div[2]/ytd-compact-video-renderer/div[1]/a")))
-                next_video = self.driver.find_element_by_xpath("/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[2]/div/div[2]/ytd-watch-next-secondary-results-renderer/div[2]/ytd-compact-autoplay-renderer/div[2]/ytd-compact-video-renderer/div[1]/a")
-                self.enable_subtitles()            
-                link = self.get_subtitles_link()
-                video_title = self.driver.find_element_by_xpath('//*[@id="video-title"]').text
-                channel_name = self.driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div/div[7]/div[3]/ytd-video-secondary-info-renderer/div/div[2]/ytd-video-owner-renderer/div[1]/div/yt-formatted-string/a').text
-            except:
-                print("video incompatible ")
-                link = ""
-                video_title = "Missing Video"
-                channel_name=""
-            print(self.start_url)
-            next_url = next_video.get_attribute("href")
-            self.start_url = next_url #go to next video in Up Next
-            yield video_title,link, channel_name if link else "No Closed Caption"
+        try:
+            self.driver.get(url)
+            self.enable_subtitles()            
+            link = self.get_subtitles_link()
+            print(link)
+            self.enable_subtitles()
+            video_title = self.driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div/div[5]/div[2]/ytd-video-primary-info-renderer/div/h1/yt-formatted-string').text
+            channel_name = self.driver.find_element_by_xpath('/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[3]/div[1]/div/div[7]/div[3]/ytd-video-secondary-info-renderer/div/div[2]/ytd-video-owner-renderer/div[1]/div/yt-formatted-string/a').text
+        except Exception as e:
+            link = ""
+            video_title = "Missing Video"
+            channel_name=""
+        yield video_title,link, channel_name if link else "No Closed Caption"
 
     def enable_subtitles(self):
         """Clicks on CC(Closed Caption) button in YouTube video."""
-        show_subtitles_button = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ytp-subtitles-button")))
-        show_subtitles_button.click()
+        try:
+            show_subtitles_button = self.wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "ytp-subtitles-button")))
+            show_subtitles_button.click()
+        except Exception as e:
+            print(e)
 
     #PROBLEM HERE
     def get_subtitles_link(self):
@@ -64,15 +87,9 @@ class YoutubeSubtitlesScraper:
         return ""
 
 if __name__ == "__main__":
-    start_url = "https://www.youtube.com/watch?v=LsqKL3pBVMA"
-    ytScraper = YoutubeSubtitlesScraper(start_url)
-    subtitles_file = open("subtitleUrl.txt",'w')
+    playlist_page = "https://www.youtube.com/results?search_query=chemistry&sp=EgIQAw%253D%253D"
+    ytScraper = YoutubeSubtitlesScraper(playlist_page)
     with ytScraper as scraper:
-        for title, link, channel in scraper.subtitles():
-            try:
-                if link != '':
-                    subtitles_file.write(title + '\t' + link + '\t' + channel + '\n')
-            except:
-                print("can't create file for: " + title + " : " + link + " : " + channel)
-    subtitles_file.close()
+        scraper.playlist_all()
+        scraper.subtitles_file.close()
 
